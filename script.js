4e67a52bcf4cd8f255dbcf9142234884874a60f5
seyi365 <feranmibello8@gmail.com>
2025-09-25 12:51:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click" , () => {
+  //toggle the mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+// close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.
+click());
+
+// close menu when the na link is clicked
+navLinks.forEach(link => {
+  link.addEventListener("click", () => menuOpenButton.click());
+})
+
+// initialize Swiper //
+const swiper = new Swiper('.slider-wrappper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true, 
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //Responsive breakpoints//
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: { 
+      slidesPerView: 3
+    }
+  }
+});
